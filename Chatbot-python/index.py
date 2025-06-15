5c22358384522214af4a649426f54dbde33eb808
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2025-06-15 12:29:39 +0530

    create simple response and request options for now

--- Chatbot-python/index.py
+++ Chatbot-python/index.py
@@ -9,10 +9,19 @@ list_to_train = [
     "hi there",
     "what is your name",
     "I'm a chatbot",
-    "how old are you?"
-    "I'm ageless"
+    "how old are you?",
+    "I'm ageless",
 ]
 
 list_trainer = ListTrainer(bot)
 
 list_trainer.train(list_to_train)
+
+while True:
+    
+    #user request
+    user_res = input("tell me something: ")
+    
+    #get response
+    bot_res = bot.get_response(user_res)
+    print("chatbot: " + str(bot_res))
